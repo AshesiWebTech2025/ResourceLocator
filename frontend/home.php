2e1b8a94cb55846975a0a4fc18a2658ab9d4d7bb
Inez Anne-Marie <eyra.agbenu@ashesi.edu.gh>
2025-11-21 11:55:16 +0000

    added the architecture page to the sidebar

--- frontend/home.php
+++ frontend/home.php
@@ -45,6 +45,7 @@
             <a href="#" id="nav-map" class="flex items-center p-3 rounded-lg hover:bg-white/10 transition duration-150 ease-in-out font-medium">Campus Map</a>
             <a href="bookings.php" class="flex items-center p-3 rounded-lg hover:bg-white/10 transition duration-150 ease-in-out font-medium">My Bookings</a>
             <a href="about.php" class="flex items-center p-3 rounded-lg hover:bg-white/10 transition duration-150 ease-in-out font-medium">About</a>
+            <a href="software_architecture.php" id="nav-architecture" class="flex items-center p-3 rounded-lg hover:bg-white/10 transition duration-150 ease-in-out font-medium">Architecture</a>
         </nav>
         <div class="p-4 space-y-2 border-t border-white/20">
             <a href="#" class="flex items-center p-3 rounded-lg hover:bg-white/10 transition duration-150 ease-in-out font-medium">Settings</a>
